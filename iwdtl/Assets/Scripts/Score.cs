5937dceb472b657bd11e2a5144d480704547a853
MikePineda <mikepineda97@gmail.com>
2018-11-21 20:59:14 -0600

    cambios peque;os para que la app sea mas de tacos

--- iwdtl/Assets/Scripts/Score.cs
+++ iwdtl/Assets/Scripts/Score.cs
@@ -18,9 +18,10 @@ public class Score : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("Moneda"))
         {
-            PlayerPrefs.SetInt("puntaje", puntaje);
+            puntaje = PlayerPrefs.GetInt("puntaje");
             other.gameObject.SetActive(false);
             puntaje = puntaje + 10;
+            PlayerPrefs.SetInt("puntaje", puntaje);
             peek.Play();
         }
 
@@ -35,6 +36,17 @@ public class Score : MonoBehaviour {
         {
             PlayerPrefs.SetInt("puntaje", puntaje);
             SceneManager.LoadScene("Dead");
+        }
+
+        if (other.gameObject.CompareTag("goLvl2"))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+
+
+        if (other.gameObject.CompareTag("goLvl3"))
+        {
+            SceneManager.LoadScene("Level3");
         }
 
         if (other.gameObject.CompareTag("Finish"))
